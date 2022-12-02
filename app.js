9e3a4bbaf333e8eafd2652487b370da5f528f222
Abdulaziz <111849967+abdulaziz200531@users.noreply.github.com>
2022-12-02 17:37:39 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// let a = prompt('son kirit')
+
+
+// if(isNaN(a)){
+//     console.log('Number Emas');
+// }else{
+//     console.log('Number');
+// }
+
+// let num = prompt ("Son Kirting");
+
+// if (num >= 0 && num <= 9){
+//     console.log("1 Xonali Son");
+// }else if (num >= 10 && num <= 99){
+//     console.log("2 Xonali Son");
+// }else if (num >= 100 && num <= 999){
+//     console.log("3 Xonali Son");
+// }else if (num >= 1000 && num <= 9999){
+//     console.log("4 Xonali Son");
+// }else{
+//     console.log("Iltimos 4 Xonali Son Kirting");
+// }
+
+
+
+// piyoda tezligi: 3.6 km/s,
+// velosiped tezligi: 20.1 km/s,
+// Mashina tezligi: 70 km/s,
+// Samalyot tezligi: 800 km/s
+
+// let km = prompt ("Km Kirting");
+
+// let car = 70;
+// let odam = 3.6;
+// let sm = 800;
+// let vel = 20.1;
+
+// console.log(`Samalyot ${km / sm} Soat ${(km % sm) / sm * 60} Minut`);
+
+// console.log( `Moshina ${Math.floor(km / car)} soat ${(km % car) / car * 60} Minut`);
+
+// console.log(`Odam ${Math.floor(km / odam)} Soat ${km % odam / odam * 60} Minut`);
+
+// console.log(`Velesapet ${Math.floor(km / vel)} Soat ${km % vel / vel * 60} Minut`);
